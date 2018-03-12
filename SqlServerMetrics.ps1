456df699bf450f479f5935da91818b7b0e84786a
aussiearef <arefkr@gmail.com>
2018-03-12 19:33:42 +1100

    Added Powershell script for sending metrics from SQL Server to Graphite

---
+++ SqlServerMetrics.ps1
@@ -0,0 +1,35 @@
+﻿param(
+  [parameter(Mandatory=$false, ValueFromPipeline=$true)]
+  [string]
+  $ip="127.0.0.1",
+  
+  [parameter(Mandatory=$false, ValueFromPipeline=$true)]
+  [string]
+  $storedProcedure="YOUR STORED PROCEDURE NAME HERE"
+)
+
+$SqlConnection = New-Object System.Data.SqlClient.SqlConnection
+$SqlConnection.ConnectionString = "Server=(local);Database=<YOUR DATABASE NAME>;user=<YOUR DB USER>;password=<YOUR DB PASSWORD>"
+$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+$SqlCmd.CommandText = $storedProcedure
+$SqlCmd.Connection = $SqlConnection
+$SqlCmd.CommandType = [System.Data.CommandType]::StoredProcedure;
+$SqlConnection.Open()
+$result = $SqlCmd.ExecuteScalar()
+$SqlConnection.Close()
+$metric = "<YOUR METRIC NAME HERE>"+$result.ToString()+"|g"
+
+
+# USE [System.Net.Dns]::GetEntries  instead, if you use a domain name rather than an IP address.
+$ipAddress=[System.Net.IPAddress]::Parse($ip) 
+
+
+$endPoint=New-Object System.Net.IPEndPoint($ipAddress, $port)
+$udpclient=New-Object System.Net.Sockets.UdpClient
+
+
+$encodedData=[System.Text.Encoding]::ASCII.GetBytes($metric)
+$bytesSent=$udpclient.Send($encodedData,$encodedData.length,$endPoint)
+
+
+$udpclient.Close()
